2c5fd7dd972a9a735cd64ea6390c0c3671c212ab
danFbach <quiktekrepair@gmail.com>
2016-03-05 17:47:47 -0600

    multi function java v1.0

--- here/src/multifunctionAdventure/LifeFunction.java
+++ here/src/multifunctionAdventure/LifeFunction.java
@@ -3,17 +3,171 @@ package multifunctionAdventure;
 public class LifeFunction {
 		
 		
-	Integer	life(Integer lifeLeft)
+	Integer	life(Integer lifeCalc)
 	{
-		switch(lifeLeft)
-		{case 1:
-			lifeLeft -= 25;
-			return lifeLeft;
-		case 2:
-			lifeLeft += 25;
-			return lifeLeft;
-		default:
-			return lifeLeft;
+		switch(lifeCalc)
+		
+		{case 1:		//1 - VIDEO
+			lifeCalc -= 20;
+			System.out.println("You watched a video about dolphins and in the process lost 20% battery life.");
+			return lifeCalc;
+			
+		case 2:			//2 - SOCIAL MEDIA
+			lifeCalc -= 15;
+			try {
+			    Thread.sleep(1500);
+			} catch(InterruptedException ex) {
+			    Thread.currentThread().interrupt();
+			}System.out.println("Self absorbed...\n");
+			try {
+			    Thread.sleep(1500);
+			} catch(InterruptedException ex) {
+			    Thread.currentThread().interrupt();
+			}System.out.println("Self absorbed...\n");
+			try {
+			    Thread.sleep(1500);
+			} catch(InterruptedException ex) {
+			    Thread.currentThread().interrupt();
+			}System.out.println("Self absorbed...\n");
+			try {
+			    Thread.sleep(1500);
+			} catch(InterruptedException ex) {
+			    Thread.currentThread().interrupt();
+			}System.out.println("Funny picture!\n");
+			try {
+			    Thread.sleep(1500);
+			} catch(InterruptedException ex) {
+			    Thread.currentThread().interrupt();
+			}System.out.println("Self absorbed...\n");
+			try {
+			    Thread.sleep(1500);
+			} catch(InterruptedException ex) {
+			    Thread.currentThread().interrupt();
+			}System.out.println("Self absorbed...\n");
+			try {
+			    Thread.sleep(1500);
+			} catch(InterruptedException ex) {
+			    Thread.currentThread().interrupt();
+			}System.out.println("Self absorbed...\n");
+			try {
+			    Thread.sleep(1500);
+			} catch(InterruptedException ex) {
+			    Thread.currentThread().interrupt();
+			}System.out.println("Funny Video!\n");
+			try {
+			    Thread.sleep(1500);
+			} catch(InterruptedException ex) {
+			    Thread.currentThread().interrupt();
+			}System.out.println("well, that's social media for you, what were you expecting...\n");
+			try {
+			    Thread.sleep(4000);
+			} catch(InterruptedException ex) {
+			    Thread.currentThread().interrupt();}
+			return lifeCalc;
+			
+		case 3:			//3 - NEWS
+			lifeCalc -= 10;
+			try {
+			    Thread.sleep(2000);
+			} catch(InterruptedException ex) {
+			    Thread.currentThread().interrupt();
+			}System.out.println("Politicians this, politicians that...\n\n");
+			try {
+			    Thread.sleep(2000);
+			} catch(InterruptedException ex) {
+			    Thread.currentThread().interrupt();
+			}System.out.println("War here, war there...\n\n");
+			try {
+			    Thread.sleep(2000);
+			} catch(InterruptedException ex) {
+			    Thread.currentThread().interrupt();
+			}System.out.println("Be careful of this, be careful of that...\n\n");
+			try {
+			    Thread.sleep(2000);
+			} catch(InterruptedException ex) {
+			    Thread.currentThread().interrupt();
+			}System.out.println("Goodnight America!");
+			try {
+			    Thread.sleep(2000);
+			} catch(InterruptedException ex) {
+			    Thread.currentThread().interrupt();}
+			return lifeCalc;	
+			
+		case 4:			//4 - BOOK
+			lifeCalc -= 5;
+			System.out.println("Congratulations dude, you actually picked on of the few things\n"
+					+ "you can do on your phone to enrich yourself. Go You.");
+			return lifeCalc;
+			
+		case 5:			//5 - GAME
+			lifeCalc -= 35;
+			System.out.println("Lets crush some crandy! or...Blow some stuff up! or...Spin the Slots!");
+			try {
+			    Thread.sleep(2000);
+			} catch(InterruptedException ex) {
+			    Thread.currentThread().interrupt();
+			}System.out.println("Lets Blow some stuff up!");			
+			try {
+			    Thread.sleep(1500);
+			} catch(InterruptedException ex) {
+			    Thread.currentThread().interrupt();
+			}System.out.println("BOOM!");			
+			try {
+			    Thread.sleep(1000);
+			} catch(InterruptedException ex) {
+			    Thread.currentThread().interrupt();
+			}System.out.println("BAKEWW! BAKEWW!");			
+			try {
+			    Thread.sleep(1000);
+			} catch(InterruptedException ex) {
+			    Thread.currentThread().interrupt();
+			}System.out.println("POW! POP POP POP!");			
+			try {
+			    Thread.sleep(1000);
+			} catch(InterruptedException ex) {
+			    Thread.currentThread().interrupt();
+			}System.out.println("BEWWWWWWWW...KAWOOOOSH!...\n"+"All of your men have died.");			
+			try {
+			    Thread.sleep(1500);
+			} catch(InterruptedException ex) {
+			    Thread.currentThread().interrupt();
+			}return lifeCalc;
+			
+		case 6:			//6 - CHARGE
+			lifeCalc += 15;
+			try {
+			    Thread.sleep(2000);
+			} catch(InterruptedException ex) {
+			    Thread.currentThread().interrupt();
+			}System.out.println("\n... + 3%");
+			try {
+			    Thread.sleep(2000);
+			} catch(InterruptedException ex) {
+			    Thread.currentThread().interrupt();
+			}System.out.println("\n... + 6%");
+			try {
+			    Thread.sleep(2000);
+			} catch(InterruptedException ex) {
+			    Thread.currentThread().interrupt();
+			}System.out.println("\n... + 9%");
+			try {
+			    Thread.sleep(2000);
+			} catch(InterruptedException ex) {
+			    Thread.currentThread().interrupt();
+			}System.out.println("\n... + 12%");
+			try {
+			    Thread.sleep(2000);
+			} catch(InterruptedException ex) {
+			    Thread.currentThread().interrupt();
+			}System.out.println("\n... + 15%");
+			try {
+			    Thread.sleep(2000);
+			} catch(InterruptedException ex) {
+			    Thread.currentThread().interrupt();
+			}return lifeCalc;
+			
+		default:			//DEFAULT
+			return lifeCalc;
 			
 		
 		}	
--- here/src/multifunctionAdventure/Main.java
+++ here/src/multifunctionAdventure/Main.java
@@ -4,37 +4,53 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		//String playerName;
-		Integer lifeLeft = 100;
+		String playerName;
+		String phone;
+		Integer choiceAns;
+		Integer lifeLeft = 100;		
+		Integer decider = 0;
 		
-		Integer decider;
+		LifeFunction choice = new LifeFunction();
 		
-	//LifeFunction life = new LifeFunction();
-		
-	@SuppressWarnings("resource")
-	Scanner scan = new Scanner(System.in);
+		@SuppressWarnings("resource")
+		Scanner scan = new Scanner(System.in);
 	
+		System.out.println("Hey...uh...whoops I already forgot your name, can you remind me?");
+		playerName = scan.nextLine();
+		System.out.println("Oh that's right, your name is " + playerName + "! Thanks for reminding me. So anyways, \n"
+					+ "here is your new phone, The Nebula X10. It features a 5.5 inch display\n"
+					+ "with a resolution of 3000x3000 pixelbits, 1 teramunch of on board storage and,\n"
+					+ "a 32 superHertz processor. Needless to say, this is the best on the market. But\n"
+					+ "there is one downfall, although it features a 10,000mah battery the incredible\n"
+					+ "power of the processor drains it somewhat fast...Are you sure you still want it?");
+			phone = scan.nextLine();
+			phone = phone.toLowerCase();
 
 		do{
-			System.out.println("1 or 2");
-			decider = scan.nextInt();
-			LifeFunction choice = new LifeFunction();
-			Integer choiceAns = choice.life(decider);
-			choiceAns += lifeLeft;
-				System.out.println(choiceAns);
-				lifeLeft = choiceAns;
+			
+			if(phone.matches("yes")){
 				
-		}while(lifeLeft > 0);{}
+				System.out.println("Your current battery life is: " + lifeLeft + "%\n\n"
+						+ "What would you like to do with you new phone "+playerName+"?\n"
+						+ "1 - Watch a Video online?\n"
+						+ "2 - See what people are doing on social media?\n"
+						+ "3 - Watch the news?\n"
+						+ "4 - Read a book?\n"
+						+ "5 - Play a game?\n"
+						+ "6 - Charge it and do nothing.");
+				decider = scan.nextInt();
+				choiceAns = choice.life(decider);
+				choiceAns += lifeLeft - decider;
+				lifeLeft = choiceAns;
+				if(lifeLeft > 100){System.out.println("Your battery is already fully charged, please try something else.");
+				lifeLeft = 100;
+				}else if(lifeLeft < 1){System.out.println("Your phone is dead.");}				
+			}else if(phone.matches("no")){
+				System.out.println("Well then leave with your flip phone NERD!");
+				lifeLeft = 0;
+				break;
+			}else System.out.println("Invalid entry.");
+			
+		}while(lifeLeft > 0);
 	}
 }
-
-	
-/*
- * System.out.println("Welcome player! I've got a fantastic bunch of events lined up for you.\n"
-		+ "Before we begin, I need something to call you. Go ahead and enter your name...for\n"
-		+ "best results first and last name is recomended.");
-playerName = scan.nextLine();
-System.out.println("Thanks " + playerName + "! We may now proceed! For our 1st event, I need to\n"
-		+ "know if you like music.\n"
-		+ "Yes or No?");
-		*/
